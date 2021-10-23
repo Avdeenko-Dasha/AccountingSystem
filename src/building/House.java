89a9456b425ee33d5569af2c186d937d033b0065
Avdeenko-Dasha <avdeenko.dasha705@gmail.com>
2021-10-24 00:44:11 +0300

    Did the functionality

--- src/building/House.java
+++ src/building/House.java
@@ -1,5 +1,7 @@
 package building;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
 
 /**
  * Class House this is...
@@ -12,14 +14,25 @@ public class House {
     private int     numHouse = 0;
     /**Variable that stores the number of floors in the house*/
     private int     numFloor = 0;
+    /**Variable that stores the number of apartments in the house*/
+    private int     numApartment = 0;
+    /**Variable that stores the number of tenants in the house*/
+    private int     numOfTenants = 0;
+    /**Variable that stores area of the house*/
+    private double     squareHouse = 0;
     /**ArrayList storing the structure of the house*/
-    private ArrayList<Floor> house = new ArrayList<Floor>();
+    private ArrayList<Floor> house;
+
+
 
     House()
     {
         numHouse = staticNumHouse;
         staticNumHouse++;
         numFloor = 0;
+        numApartment = 0;
+        numOfTenants = 0;
+        squareHouse = 0;
         house = new ArrayList<Floor>(0);
         Floor floor = new Floor();
         floor.setStaticNumFloor(1);
@@ -31,8 +44,20 @@ public class House {
         return numFloor;
     }
 
-    public void setNumFloor(int numFloor) {
-        this.numFloor = numFloor;
+    public int getNumApartment() {
+        return numApartment;
+    }
+
+    public int getNumHouse() {
+        return numHouse;
+    }
+
+    public int getNumOfTenants() {
+        return numOfTenants;
+    }
+
+    public double getSquareHouse() {
+        return squareHouse;
     }
 
     /**
@@ -59,12 +84,13 @@ public class House {
     }
 
     public String toString() {
-        StringBuilder str = new StringBuilder();
+        StringBuilder str = new StringBuilder("House " + numHouse + ":" + "\n");
         for(int i = 0; i < numFloor; ++i){
             str.append(house.get(i).toString());
             str.append("\n");
         }
-        return "House " + numHouse + ":" + "\n" + str.toString();
+        str.append("Square - " + getSquareHouse() + "\nNumber of tenants - " + getNumOfTenants() + "\n");
+        return str.toString();
     }
 
     /**
@@ -116,6 +142,22 @@ public class House {
         } else return false;
     }
 
+    private static int enterNumInt(){
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        while(sc.hasNext()){
+            if(sc.hasNextInt()){
+                n = sc.nextInt();
+                break;
+            }
+            else {
+                System.out.println("Enter the number!");
+                sc.next();
+            }
+        }
+        return n;
+    }
+
     public static class BuilderHouse{
         private House newHouse;
 
@@ -125,13 +167,36 @@ public class House {
 
         public BuilderHouse setNumFloor(int numFloor){
             newHouse.numFloor = numFloor;
-            newHouse.house = new ArrayList<Floor>(numFloor);
-            int numApartment = (int)(2 + Math.random() * 4);
-            for(int i = 0; i < numFloor; ++i){
-                Floor floor = new Floor.BuilderFloor().setNumApartment(numApartment).build();
-                newHouse.house.add(i, floor);
-            }
             return this;
+        }
+        public BuilderHouse methodOfCreation(String method){
+            if(method.toLowerCase(Locale.ROOT).equals("yourself")){
+
+                newHouse.house = new ArrayList<>(newHouse.numFloor);
+                int sumApartment = 0;
+                for(int i = 0; i < newHouse.numFloor; ++i){
+                    System.out.print("Enter the number of apartments on the " + i + " floor");
+                    int numApartment = enterNumInt();
+                    sumApartment += numApartment;
+                    Floor floor = new Floor.BuilderFloor().setNumApartment(numApartment).methodOfCreation("yourself").build();
+                    newHouse.house.add(i, floor);
+                }
+                newHouse.numApartment = sumApartment;
+            }else
+            {
+                newHouse.house = new ArrayList<>(newHouse.numFloor);
+                int sumApartment = 0;
+                int numApartment = (int)(2 + Math.random() * 4);
+                for(int i = 0; i < newHouse.numFloor; ++i){
+                    sumApartment += numApartment;
+                    Floor floor = new Floor.BuilderFloor().setNumApartment(numApartment).methodOfCreation("automatically").build();
+                    newHouse.house.add(i, floor);
+                }
+                newHouse.numApartment = sumApartment;
+            }
+            newHouse.squareHouse = newHouse.calculateArea();
+            newHouse.numOfTenants = newHouse.countTenants();
+                return this;
         }
 
         public House build(){
--- src/building/Main.java
+++ src/building/Main.java
@@ -1,28 +1,313 @@
 package building;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Enter the number of Houses: ");
-        int numHouse = enterNum();
-        for(int i = 0; i < numHouse; ++i) {
-            System.out.print("Enter the number of floors in the house: ");
-            int numFloor = enterNum();
-            House house = new House.BuilderHouse().setNumFloor(numFloor).build();
-            System.out.println(house.toString());
-            System.out.println("House square - " + house.calculateArea());
-            System.out.println("Number of residents in the house - " + house.countTenants());
-            System.out.println("Number of floors in the house - " + house.getNumFloor());
-            System.out.println();
+        ArrayList<House> arrayHouses = new ArrayList<>(0);
+        ArrayList<Apartment> arrayApartment = new ArrayList<>(0);
+        System.out.println("Welcome to the accounting system!");
+        while (true) {
+            System.out.println("1 - Create a house\n2 - Display information about the house\n3 - All created houses");
+            System.out.println("4 - Create a apartment\n5 - Display information about the apartment\n6 - All created apartments");
+            System.out.println("7 - Compare objects\n0 - Exit");
+            System.out.print("Enter the number of the operation you want to perform - ");
+            int button = enterNumInt();
+            if (button == 0)
+                break;
+            while (button < 0 || button >= 8) {
+                System.out.print("There is no such operation\nTry again - ");
+                button = enterNumInt();
+            }
+            if (button == 0)
+                break;
+
+            switch (button) {
+                case 1:
+                    createNewHouse(arrayHouses);
+                    break;
+                case 2:
+                    if (arrayHouses.size() != 0)
+                        outputHouse(arrayHouses);
+                    else
+                        System.out.println("You don't have any homes created!");
+                    break;
+                case 3:
+                    if (arrayHouses.size() != 0)
+                        outputAllHouse(arrayHouses);
+                    else
+                        System.out.println("You don't have any homes created!");
+                    break;
+                case 4:
+                    createNewApartment(arrayApartment);
+                    break;
+                case 5:
+                    if (arrayApartment.size() != 0)
+                        outputApartment(arrayApartment);
+                    else
+                        System.out.println("You don't have any apartments created!");
+                    break;
+                case 6:
+                    if (arrayApartment.size() != 0)
+                        outputAllApartment(arrayApartment);
+                    else
+                        System.out.println("You don't have any apartments created!");
+                    break;
+                case 7:
+                    compare(arrayApartment, arrayHouses);
+                    break;
+            }
+        }
+        System.out.println("Goodbye. Good luck!");
+    }
+
+    public static void createNewHouse(ArrayList<House> arrayHouse) {
+        while (true) {
+            System.out.println("How do you want to create a house?\n1 - Yourself\n2 - Automatically\n0 - Exit");
+            int button = enterNumInt();
+            while (button < 0 || button > 2) {
+                System.out.print("There is no such operation\nTry again - ");
+                button = enterNumInt();
+            }
+            if (button == 0) {
+                break;
+            }
+            int numFloor = 0;
+            switch (button) {
+                case 1:
+                    System.out.print("Enter the number of floors - ");
+                    numFloor = enterNumInt();
+                    House newHouse = new House.BuilderHouse().setNumFloor(numFloor).methodOfCreation("yourself").build();
+                    arrayHouse.add(newHouse);
+                    break;
+                case 2:
+                    numFloor = (int) (2 + Math.random() * 10);
+                    House newHouses = new House.BuilderHouse().setNumFloor(numFloor).methodOfCreation("automatically").build();
+                    arrayHouse.add(newHouses);
+                    break;
+            }
+            System.out.println("Do you want to make another house?\n1 - Yes\n0 - No");
+            button = enterNumInt();
+            if (button == 0)
+                break;
         }
     }
 
-    public static int enterNum(){
+    public static void outputHouse(ArrayList<House> arrayHouse){
+        System.out.print("Enter the number of the house you want to get information about - ");
+        int index = enterNumInt();
+        while(index <= 0 || index >= arrayHouse.size()){
+            System.out.print("There is no house with this number, try again - ");
+            index = enterNumInt();
+        }
+        while(true) {
+            System.out.println("What information do you want to get?");
+            System.out.println("1 - Number of floors\n2 - Number of apartments\n3 - Number of tenants");
+            System.out.println("4 - Total area of the house\n5 - All information about each apartment\n0 - Exit");
+            System.out.print("Enter the number of the operation you want to perform - ");
+            int button = enterNumInt();
+            if(button == 0)
+                break;
+            while(button < 0 || button >= 6){
+                System.out.print("There is no such operation\nTry again - ");
+                button = enterNumInt();
+            }
+            if(button == 0)
+                break;
+
+            switch(button){
+                case 1:
+                    System.out.println("Number of floors - " + arrayHouse.get(index - 1).getNumFloor());
+                    break;
+                case 2:
+                    System.out.println("Number of apartments - " + arrayHouse.get(index - 1).getNumApartment());
+                    break;
+                case 3:
+                    System.out.println("Number of tenants - " + arrayHouse.get(index - 1).getNumOfTenants());
+                    break;
+                case 4:
+                    System.out.println("Total area of the house - " + arrayHouse.get(index - 1).getSquareHouse());
+                    break;
+                case 5:
+                    System.out.println("All information\n" + arrayHouse.get(index - 1).toString());
+                    break;
+            }
+            System.out.println("Do you want to display any more information?\n1 - Yes\n0 - No");
+            button = enterNumInt();
+            if(button == 0)
+                break;
+        }
+
+    }
+
+    public static void outputAllHouse(ArrayList<House> arrayHouse){
+        for(int i = 0; i < arrayHouse.size(); ++i){
+            System.out.println("House number - " + arrayHouse.get(i).getNumHouse() + "   Square - " + arrayHouse.get(i).getSquareHouse() + "   Number of tenants - " + arrayHouse.get(i).getNumOfTenants());
+        }
+    }
+
+    public static void createNewApartment(ArrayList<Apartment> arrayApartment) {
+        Apartment apartment = new Apartment();
+        apartment.setStaticNumApartment(1);
+        while (true) {
+            System.out.println("How do you want to create a apartment?\n1 - Yourself\n2 - Automatically\n0 - Exit");
+            int button = enterNumInt();
+            while (button < 0 || button > 2) {
+                System.out.print("There is no such operation\nTry again - ");
+                button = enterNumInt();
+            }
+            if (button == 0) {
+                break;
+            }
+            double square = 0;
+            int numResidents = 0;
+            switch (button) {
+                case 1:
+                    System.out.print("Enter the area of apartment - ");
+                    square = enterNumDouble();
+                    System.out.print("Enter the number of tenants in the apartment - ");
+                    numResidents = enterNumInt();
+
+                    Apartment apartment1 = new Apartment.BuilderApartment().setSquareApartment(square).setNumResidents(numResidents).build();
+                    arrayApartment.add(apartment1);
+                    break;
+                case 2:
+                    square = 30 + Math.random() * 200;
+                    numResidents = (int) (Math.random() * 5);
+
+                    Apartment apartment2 = new Apartment.BuilderApartment().setSquareApartment(square).setNumResidents(numResidents).build();
+                    arrayApartment.add(apartment2);
+                    break;
+            }
+            System.out.println("Do you want to make another apartment?\n1 - Yes\n0 - No");
+            button = enterNumInt();
+            if(button == 0)
+                break;
+        }
+    }
+
+    public static void outputApartment(ArrayList<Apartment> arrayApartment){
+        System.out.print("Enter the number of the apartment you want to get information about - ");
+        int index = enterNumInt();
+        while(index <= 0 || index >= arrayApartment.size()){
+            System.out.print("There is no house with this number, try again - ");
+            index = enterNumInt();
+        }
+        while(true) {
+            System.out.println("What information do you want to get?");
+            System.out.println("1 - Number of tenants");
+            System.out.println("2 - Square\n3 - All information about apartment\n0 - Exit");
+            System.out.print("Enter the number of the operation you want to perform - ");
+            int button = enterNumInt();
+            if(button == 0)
+                break;
+            while(button < 0 || button >= 4){
+                System.out.print("There is no such operation\nTry again - ");
+                button = enterNumInt();
+            }
+            if(button == 0)
+                break;
+
+            switch(button){
+                case 1:
+                    System.out.println("Number of tenants - " + arrayApartment.get(index - 1).getNumResidents());
+                    break;
+                case 2:
+                    System.out.println("Square - " + arrayApartment.get(index - 1).getSquareApartment());
+                    break;
+                case 3:
+                    System.out.println("All information\n" + arrayApartment.get(index - 1).toString());
+                    break;
+            }
+            System.out.println("Do you want to display any more information?\n1 - Yes\n0 - No");
+            button = enterNumInt();
+            if(button == 0)
+                break;
+        }
+    }
+
+    public static void outputAllApartment(ArrayList<Apartment> arrayApartment){
+        for(int i = 0; i < arrayApartment.size(); ++i){
+            System.out.println("Apartment number - " + i + "   Square - " + arrayApartment.get(i).getSquareApartment() + "   Number of tenants - " + arrayApartment.get(i).getNumResidents());
+        }
+    }
+
+    public static void compare(ArrayList<Apartment> arrayApartment, ArrayList<House> arrayHouse){
+        while(true) {
+            System.out.println("What will you compare?\n1 - Houses\n2 - Apartments\n0 - Exit");
+            int button = enterNumInt();
+            if(button == 0)
+                break;
+            while(button < 0 || button >= 3) {
+                System.out.print("There is no such operation\nTry again - ");
+                button = enterNumInt();
+            }
+            if(button == 0)
+                break;
+
+            if(button == 1){
+                if(arrayHouse.size()>1) {
+                    System.out.print("Enter the number of the first house - ");
+                    int index1 = enterNumInt();
+                    while (index1 <= 0 || index1 >= arrayHouse.size()) {
+                        System.out.print("There is no house with this number, try again - ");
+                        index1 = enterNumInt();
+                    }
+                    System.out.print("Enter the number of the second house - ");
+                    int index2 = enterNumInt();
+                    while (index2 <= 0 || index2 >= arrayHouse.size()) {
+                        System.out.print("There is no house with this number, try again - ");
+                        index2 = enterNumInt();
+                    }
+                    arrayHouse.get(index1-1).compare(arrayHouse.get(index2-1));
+                }
+                else System.out.println("Not enough houses to compare");
+            } else {
+                if(arrayApartment.size()>1) {
+                    System.out.print("Enter the number of the first apartment - ");
+                    int index1 = enterNumInt();
+                    while (index1 <= 0 || index1 >= arrayHouse.size()) {
+                        System.out.print("There is no apartment with this number, try again - ");
+                        index1 = enterNumInt();
+                    }
+                    System.out.print("Enter the number of the second apartment - ");
+                    int index2 = enterNumInt();
+                    while (index2 <= 0 || index2 >= arrayHouse.size()) {
+                        System.out.print("There is no apartment with this number, try again - ");
+                        index2 = enterNumInt();
+                    }
+                    arrayApartment.get(index1-1).compare(arrayApartment.get(index2-1));
+                }
+                else System.out.println("Not enough apartments to compare");
+            }
+            System.out.println("Do you want to compare objects again?\n1 - Yes\n0 - No");
+            button = enterNumInt();
+            if(button==0)
+                break;
+        }
+    }
+
+    public static int enterNumInt(){
         Scanner sc = new Scanner(System.in);
         int n = 0;
         while(sc.hasNext()){
             if(sc.hasNextInt()){
                 n = sc.nextInt();
+                break;
+            }
+            else {
+                System.out.println("Enter the number!");
+                sc.next();
+            }
+        }
+        return n;
+    }
+
+    public static double enterNumDouble(){
+        Scanner sc = new Scanner(System.in);
+        double n = 0;
+        while(sc.hasNext()){
+            if(sc.hasNextDouble()){
+                n = sc.nextDouble();
                 break;
             }
             else {
